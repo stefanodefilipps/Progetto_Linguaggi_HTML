c76dc16b8bc743a375655a5824a74d6c51a86ff1
Ingegneria Informatica e Automatica <stfn.defilippis@gmail.com>
2017-11-21 23:02:55 +0100

    Spostato pulsante "più" nella colonna dei post e modifica di quest'ultimo. Aggiunti di effetti grafici tramite JQueary con cui faccio apparire e sparire la form per inserire un nuovo post. Aggiunta effetto grafico della modifica della classe dell'icona sempre sul click e aggiunto con JQueary. Aggiunta di un nuovo file Script in cui incominciamo a mettere tutti gli script che vengono poi usati nelle pagine html

---
+++ LTW_Prog/Script/home.js
@@ -0,0 +1,7 @@
+$("document").ready(function(){
+            $("#icona").click(function(){
+                $("#form").fadeToggle("slow");
+                $("#icona").toggleClass("fa fa-plus-circle");
+                $("#icona").toggleClass("fa fa-minus-circle")
+            })
+        })
